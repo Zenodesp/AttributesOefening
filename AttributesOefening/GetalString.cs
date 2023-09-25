cde8ea76b6a2a450cd5ecef03f51d97c8d0cc86c
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2023-09-25 14:38:17 +0200

    Add project files.

---
+++ AttributesOefening/GetalString.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace AttributesOefening
+{
+    internal class GetalString
+    {
+        public string Getal { get { input = string.Concat(output.Where(Char.IsDigit)); return input; } set { output = value; } }
+
+        public string input;
+        public string output;
+        public GetalString() { }
+
+        public GetalString(string getal) {
+            this.Getal = getal;
+        }
+
+        public override string ToString()
+        {
+            return Getal;
+        }
+    }
+}
---
+++ AttributesOefening/Program.cs
@@ -0,0 +1,14 @@
+﻿using AttributesOefening;
+
+string getal;
+
+
+
+Console.WriteLine("Geef uw getal in. Letters in uw input zullen verwijderd worden.");
+getal = Console.ReadLine();
+
+if (getal.Any(char.IsDigit))
+{
+    GetalString Teste = new GetalString(getal);
+    Console.WriteLine(Teste.ToString());
+}
